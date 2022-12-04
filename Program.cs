a8250a699b50af2933cb301f43c41f3845f0eaee
ricosetti <ricosett@gmail.com>
2022-12-04 02:39:50 +0100

    main

--- Program.cs
+++ Program.cs
@@ -1,5 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello,1 World!");
 for (int i = 0; i < 10; i++)
 {
     Console.WriteLine(i);
